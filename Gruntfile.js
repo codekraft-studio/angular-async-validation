66c2e6c9023bb709f956ca07375dc0965336aa7a
codekraft-studio <info@codekraft.it>
2017-05-04 10:52:11 +0200

    connect, ngAnnotate tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,98 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-        jshint: {
-            all: ['Gruntfile.js', 'src/**/*.js']
-        },
+    jshint: {
+      gruntfile: ['Gruntfile.js'],
+      scripts: ['src/**/*.js']
+    },
 
-        concat: {
-            options: {
-                banner: '/**\n' +
-                        '* Package: <%= pkg.name %> - v<%= pkg.version %> \n' +
-                        '* Description: <%= pkg.description %> \n' +
-        				'* Last build: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
-						'* @author <%= pkg.author %> \n' +
-						'* @license <%= pkg.license %> \n'+
-                        '*/\n'
-            },
-            dist: {
-                src: ['src/<%= pkg.name %>.module.js', 'src/<%= pkg.name %>.*.js'],
-                dest: 'dist/<%= pkg.name %>.js',
-            }
-        },
+    concat: {
+      options: {
+        banner: '/**\n' +
+        '* Package: <%= pkg.name %> - v<%= pkg.version %> \n' +
+        '* Description: <%= pkg.description %> \n' +
+        '* Last build: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+        '* @author <%= pkg.author %> \n' +
+        '* @license <%= pkg.license %> \n'+
+        '*/\n'
+      },
+      dist: {
+        src: ['src/<%= pkg.name %>.module.js', 'src/<%= pkg.name %>.*.js'],
+        dest: 'dist/<%= pkg.name %>.js',
+      }
+    },
 
-        uglify: {
-            options: {
-                mangle: false,
-                banner: '/**\n' +
-                        '* Package: <%= pkg.name %> - v<%= pkg.version %> \n' +
-                        '* Description: <%= pkg.description %> \n' +
-        				'* Last build: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
-						'* @author <%= pkg.author %> \n' +
-						'* @license <%= pkg.license %> \n'+
-                        '*/\n'
-            },
-            dist: {
-                files: {
-                    'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
-                }
-            }
-        },
-
-        watch: {
-            scripts: {
-                files: ['src/**/*.js'],
-                tasks: ['build'],
-                options: {
-                  spawn: false,
-                }
-            }
+    uglify: {
+      options: {
+        mangle: false,
+        banner: '/**\n' +
+        '* Package: <%= pkg.name %> - v<%= pkg.version %> \n' +
+        '* Description: <%= pkg.description %> \n' +
+        '* Last build: <%= grunt.template.today("yyyy-mm-dd") %> \n' +
+        '* @author <%= pkg.author %> \n' +
+        '* @license <%= pkg.license %> \n'+
+        '*/\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js']
         }
+      }
+    },
 
-    });
+    ngAnnotate: {
+      options: {
+        singleQuotes: true,
+      },
+      dist: {
+        files: { 'dist/<%= pkg.name %>.js': ['dist/<%= pkg.name %>.js'] }
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    watch: {
+      gruntfile: {
+        files: ['Gruntfile.js'],
+        tasks: ['jshint:gruntfile']
+      },
+      scripts: {
+        files: ['src/**/*.js'],
+        tasks: ['build'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    },
 
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['jshint', 'concat', 'uglify']);
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          open: true,
+          livereload: true,
+          base: {
+            path: '.',
+            options: {
+              index: 'example/index.html'
+            }
+          }
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-ng-annotate');
+
+  grunt.registerTask('default', ['connect', 'watch']);
+  grunt.registerTask('build', ['jshint', 'concat', 'ngAnnotate', 'uglify']);
 
 };
